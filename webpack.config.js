0d71f89e996b285118acc631ae3bc003f4442982
Sher Minn Chong <sherminn.chong@gmail.com>
2015-10-14 16:47:44 -0400

    Fix paths

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'public'),
-        publicPath: 'mta-maps/public/',
+        publicPath: '/',
         filename: 'bundle.js'
     },
     devtool: 'eval',
