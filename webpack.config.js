cb76843a53720dcbaace2a0a7eaa55edee77dd6a
Sher Minn Chong <sherminn.chong@gmail.com>
2015-10-14 16:43:22 -0400

    Rebuild with public/ path

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'public'),
-        publicPath: '/',
+        publicPath: 'public/',
         filename: 'bundle.js'
     },
     devtool: 'eval',
