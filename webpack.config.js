8b8310597667aacce421b76ae35b778148490fc6
Sher Minn Chong <sherminn.chong@gmail.com>
2015-10-14 16:44:06 -0400

    Rebuild with mta-maps/public/ path

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'public'),
-        publicPath: 'public/',
+        publicPath: 'mta-maps/public/',
         filename: 'bundle.js'
     },
     devtool: 'eval',
