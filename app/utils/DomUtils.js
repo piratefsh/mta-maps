79afe2c4d397c26fc159b14224f9f9610ba2ebbe
Sher Minn Chong <sherminn.chong@gmail.com>
2015-10-15 15:22:42 -0400

    Add utils

---
+++ app/utils/DomUtils.js
@@ -0,0 +1,30 @@
+export default {
+        initToggle(elem, options){
+            /** Creates a toggle button
+                    options = {
+                        state: () => 'function to test if toggle is on. 
+                             returns true if is on, false if otherwise',      
+                        on: {
+                            text: 'innerHTML to show when toggle on'
+                            callback: 'thing to do when toggle on'
+                        },
+                        off: {
+                            text: 'innerHTML to show when toggle off'
+                        callback: 'thing to do when toggle off'
+                        }
+                    }
+            **/
+            elem.onclick = e => {
+                //on
+                if (options.state() == true) {
+                    options.on.callback()
+                    elem.innerHTML = options.on.text
+                }
+                //off
+                else{
+                    options.off.callback()
+                    elem.innerHTML = options.off.text
+                }
+            } 
+        }
+}
