a3268fc410ac82da808e7a01c0ae4cfada92df0d
Sher Minn Chong <sherminn.chong@gmail.com>
2015-10-15 12:33:19 -0400

    Make glow and show station toggle

--- app/components/MapControls.js
+++ app/components/MapControls.js
@@ -1,18 +1,19 @@
 export default class MapControls{
     constructor(map){
         this.map = map 
+        this.stationsVisible = true
+        this.removeLayer = (linename, layer) => {
+            this.map.removeLayer(layer)
+        }
     }
 
     showLines(lines){
-        const r = (linename,layer) => {
-            this.map.removeLayer(layer)
-        }
+        this.showStations(lines)
+        this.showHeats(lines)
+    }
 
-        this.map.eachLineLayer(r)
-        this.map.eachHeatLayer('entries', r)
-        this.map.eachHeatLayer('exits', r)
-
-        const f = (linename,layer) => {
+    showHeats(lines){
+        const showL = (linename, layer) => {
             lines.forEach(l => {
                 if(linename.indexOf(l) > -1){
                     layer.addTo(this.map).bringToFront()
@@ -20,8 +21,33 @@ export default class MapControls{
             })
         }
 
-        this.map.eachLineLayer(f)
-        this.map.eachHeatLayer('entries', f)
-        this.map.eachHeatLayer('exits', f)
+        this.map.eachHeatLayer('entries', this.removeLayer)
+        this.map.eachHeatLayer('exits', this.removeLayer)
+        this.map.eachHeatLayer('entries', showL)
+        this.map.eachHeatLayer('exits', showL)
     }
+
+    showStations(lines){
+        if(this.stationsVisible) {
+            this.map.eachLineLayer(this.removeLayer)
+            this.map.eachLineLayer((linename, layer) => {
+                lines.forEach(l => {
+                    if(linename.indexOf(l) > -1){
+                        layer.addTo(this.map).bringToFront()
+                    }
+                })
+            })
+        }
+    }
+
+    toggleStations(show, lines){
+        this.stationsVisible = show
+        if(show){
+            this.showStations(lines)
+        }
+        else{
+            this.map.eachLineLayer((name, layer) => this.map.removeLayer(layer))
+        }
+    }
+
 }
